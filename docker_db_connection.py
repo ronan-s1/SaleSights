fb9c99f03126dba4bdeb28936ffb8203f2905776
ronan-s1 <singpurwala13@gmail.com>
2023-12-04 22:46:01 +0000

    so you dont accidentally build with localhost

--- docker_db_connection.py
+++ docker_db_connection.py
@@ -5,8 +5,9 @@ import textwrap
 def is_running_in_docker():
     path = "/proc/self/cgroup"
     return (
-        os.path.exists("/.dockerenv") or
-        os.path.isfile(path) and any("docker" in line for line in open(path))
+        os.path.exists("/.dockerenv")
+        or os.path.isfile(path)
+        and any("docker" in line for line in open(path))
     )
 
 
@@ -29,10 +30,10 @@ def update_secrets_file():
         """
         )
         print("Secrets file updated for local env.")
-        
+
     with open(secrets_path, "w") as secrets_file:
         secrets_file.write(secrets_content)
-    
+
 
 if __name__ == "__main__":
     update_secrets_file()
