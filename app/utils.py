986670a6472fcabc1a3c0b96a73fbc904e8d6ea0
ronan-s1 <singpurwala13@gmail.com>
2024-01-25 22:26:46 +0000

    shared data in utils

---
+++ app/utils.py
@@ -0,0 +1,6 @@
+import os
+
+
+LOGO_PATH = os.path.join("app", "static", "img", "salesights-logo.png")
+LOGO_ICON_PATH = os.path.join("app", "static", "img", "salesights-title-icon.png")
+CSS_PATH = os.path.join("app", "static", "css", "style.css")
