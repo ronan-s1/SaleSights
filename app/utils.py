91f06b6783e4ce21efbe246a8a06de0fef18a67a
ronan-s1 <singpurwala13@gmail.com>
2024-02-13 01:14:59 +0000

    vspace

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,14 @@
 import os
 
+import streamlit as st
+
 
 LOGO_PATH = os.path.join("app", "static", "img", "salesights-logo.png")
 LOGO_ICON_PATH = os.path.join("app", "static", "img", "salesights-title-icon.png")
 CSS_PATH = os.path.join("app", "static", "css", "style.css")
 FIG_BLUE_BACKGROUND_COLOUR = "#f7f8ff"
+
+
+def v_spacer(height):
+    for _ in range(height):
+        st.write("\n")
